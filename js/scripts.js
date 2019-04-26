cb4c2185336bee9393812b40b89be4fe3dc2b1b5
Alan <mashalonzo741@gmail.com>
2019-04-26 16:24:49 +0300

    add changes in the script file

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
 function result() {
-  var sidea = parseInt(getElementById('fvalue'));
-  var sideb = parseInt(getElementById('svalue'));
-  var sidec = parseInt(getElementById('tvalue'))
+  var sidea = parseInt(document.getElementById('fvalue'));
+  var sideb = parseInt(document.getElementById('svalue'));
+  var sidec = parseInt(document.getElementById('tvalue'))
+
+  if (sidea+sideb<=sidec||sideb+sidec<=sidea||sidea+sidec<=sideb) {
+    document.getElementById('result').innerHTML="This values do NOT make a triangle"
+  }
+  else if (sidea===sideb && sidea===sidec) {
+    document.getElementById('result').innerHTML="This is an equilateral triangle"
+  }
+  else if (sidea!==sideb && sideb!==sidec && sidea!==sidec) {
+    document.getElementById('result').innerHTML="This is a scalene triangle"
+  }
+  else (sidea===sideb || sideb===sidec || sidea===sidec){
+    document.getElementById('result').innerHTML="This is an isosceles triangle"
+  }
 }
